6dd9e2b5b885486787f3fc42e9a8072c90932ce8
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2022-12-04 22:03:53 +0300

    Add files via upload

---
+++ easy_str1.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string zaim(string nn) {
+    string rev;
+    for (int i = nn.size() - 1; i >= 0; i--) {
+        rev = rev.append(1, nn[i]);
+    };
+    return rev;
+}
+
+string itc_hello_str(string name) { //1+
+    cin >> name;
+    string a = "Hello, ";
+    a = a + name;
+    return a;
+}
+
+long long itc_len(string str) { //2+
+    char cent;
+    long long iiiiigor = 0;
+    while (str != "") {
+        str = zaim(str);
+        cent = str[0];
+        str = zaim(str);
+        str.pop_back();
+        iiiiigor++;
+    }
+    return iiiiigor;
+}
+
+void itc_print_copy_str(string str, int number) { //3+
+    while (number > 0) {
+        cout << str << endl;
+        number = number - 1;
+    }
+    return;
+}
+
+void itc_first_end_three(string str) { //4+
+    long long a;
+    string soska = str;
+    char cent;
+    while (str > "") {
+        str.pop_back();
+        a++;
+    }
+    if (a <= 5) {
+        cent = soska[0];
+        while (a > 0) {
+            cout << cent;
+            a = a - 1;
+        }
+        return;
+    }
+    cent = soska[0];
+    cout << cent;
+    cent = soska[1];
+    cout << cent;
+    cent = soska[2];
+    cout << cent;
+    soska = zaim(soska);
+    cent = soska[2];
+    cout << cent;
+    cent = soska[1];
+    cout << cent;
+    cent = soska[0];
+    cout << cent;
+}
---
+++ easy_str2.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int itc_count_char_in_str(char ch, string str){ //5+
+    char cent;
+    long long geniq;
+    geniq = 0;
+    while(str > ""){
+        cent = str[0];
+        str = zaim(str);
+        str.pop_back();
+        str = zaim(str);
+        if(cent == ch){
+            geniq++;
+        }else
+            geniq = geniq;
+    }
+    return geniq;
+}
+
+string itc_even_place(string str){ //6+
+    char cent;
+    string Qineg;
+    while(str > ""){
+        cent = str[0];
+        str = zaim(str);
+        str.pop_back();
+        str = zaim(str);
+        if(cent == '0' || cent == '1' || cent == '2' || cent == '3' || cent == '4' || cent == '5' || cent == '6' || cent == '7' || cent == '8' || cent == '9'){
+            Qineg+= cent;
+        }
+    }
+    if(Qineg == ""){
+        cout << -1;
+        return 0;
+    }
+    cout << Qineg;
+}
+
+double itc_percent_lower_uppercase(string str){ //7+
+    char cent;
+    double sera = 0;
+    double sira = 0;
+    while(str > ""){
+        cent = str[0];
+        str = zaim(str);
+        str.pop_back();
+        str = zaim(str);
+        if(cent == 'a' || cent == 'b' || cent == 'c' || cent == 'd' || cent == 'e' || cent == 'f' || cent == 'g' || cent == 'h' || cent == 'i' || cent == 'j' || cent == 'k' || cent == 'l' || cent == 'm' || cent == 'n' || cent == 'o' || cent == 'p' || cent == 'q' || cent == 'r' || cent == 's' || cent == 't' || cent == 'u' || cent == 'v' || cent == 'w' || cent == 'x' || cent == 'y' || cent == 'z'){
+            sira++;
+        }else
+        if(cent == 'A' || cent == 'B' || cent == 'C' || cent == 'D' || cent == 'E' || cent == 'F' || cent == 'G' || cent == 'H' || cent == 'I' || cent == 'J' || cent == 'K' || cent == 'L' || cent == 'M' || cent == 'N' || cent == 'O' || cent == 'P' || cent == 'Q' || cent == 'R' || cent == 'S' || cent == 'T' || cent == 'U' || cent == 'V' || cent == 'W' || cent == 'X' || cent == 'Y' || cent == 'Z'){
+            sera++;
+        }
+    }
+    if(sira > sera){
+        sira = sera / sira * 100;
+    }else
+        sira = sira / sera * 100;
+    return sira;
+}
+
+string itc_reverse_str(string str){ //8+
+    str = zaim(str);
+    cout << str;
+}
+
+string itc_slice_str(string str, int start, int end){ //9
+    long long sas = itc_len(str);
+    if(sas < start){
+        str+= "\0";
+        cout << str;
+    }else
+    if(end > sas){
+        while(start > 0){
+            str = zaim(str);
+            str.pop_back();
+            str = zaim(str);
+            start--;
+        }
+        str+= "\0";
+        cout << str;
+        return 0;
+    }else
+    if(sas > end){
+        while(start > 0){
+            str = zaim(str);
+            str.pop_back();
+            str = zaim(str);
+            start--;
+        }
+        str = zaim(str);
+        sas-= end;
+        while(sas > 0){
+            str = zaim(str);
+            str.pop_back();
+            str = zaim(str);
+            sas--;
+        }
+        str = zaim(str);
+        str+= "\0";
+        cout << str;
+    }
+}
